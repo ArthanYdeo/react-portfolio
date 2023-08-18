0e0d3c505c63b49dcb8871ccd331d50977e04ca2
Arthan Kyle Job Ydeo <ares.ydeo.up@phinmaed.com>
2023-08-19 02:21:23 +0800

    added stuffs.

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './works.css'
+import Works1 from '../../assets/paykana.jpg'
 
 const Works = () => {
   return (
     <section id='works'>
        <h2 className='worksTitle'>My Portfolio</h2>
-       <span className='worksDescription'>My works as a Web Development Student.</span>
+       <span className='worksDescription'>My work as a Web Development Student.</span>
        <div className='worksImgs'>
-        <img src='' className='worksImg'></img>
-        <img src='' className='worksImg'></img>
-        <img src='' className='worksImg'></img>
-        <img src='' className='worksImg'></img>
-
+        <img src={Works1} className='worksImg'></img>
+        <h3>Pay Kana</h3>
+        <p>Description:</p>
        </div>
     </section>
   )
